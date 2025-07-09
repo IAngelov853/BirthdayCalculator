667169cadf99a65a6da343c5c53e0fe6f5d4d552
IAngelov853 <lolicholi93@gmail.com>
2025-07-09 10:26:57 +0300

    Add files via upload

---
+++ BirthdayCalculator/BirthdayCalculator/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace BirthdayCalculator
+{
+
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            labelResult = new Label();
+            buttonClear = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Info;
+            button1.Location = new Point(473, 447);
+            button1.Name = "button1";
+            button1.Size = new Size(311, 56);
+            button1.TabIndex = 0;
+            button1.Text = "Изчисли";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(525, 162);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(525, 220);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 2;
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelResult.Location = new Point(584, 321);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(82, 20);
+            labelResult.TabIndex = 3;
+            labelResult.Text = "labelResult";
+            labelResult.Click += button1_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(571, 418);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(108, 23);
+            buttonClear.TabIndex = 4;
+            buttonClear.Text = "Изчисти";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1277, 645);
+            Controls.Add(buttonClear);
+            Controls.Add(labelResult);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+        private Button buttonClear;
+    }
+}
---
+++ BirthdayCalculator/BirthdayCalculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BirthdayCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            DateTime startDate = dateTimePicker1.Value.Date;
+            DateTime endDate = dateTimePicker2.Value.Date;
+            if (endDate < startDate)
+            {
+                MessageBox.Show("Втората дата трябва да е след първата!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // спира изпълнението на метода, за да няма грешка
+            }
+            TimeSpan difference = endDate - startDate;
+            int days = Math.Abs(difference.Days);
+
+            int years = endDate.Year - startDate.Year;
+            if (endDate < startDate.AddYears(years))
+            {
+                years--;
+            }
+
+
+            DateTime today = DateTime.Today;
+            DateTime birthdayThisYear = new DateTime(today.Year, startDate.Month, startDate.Day);
+            if (birthdayThisYear < today)
+                birthdayThisYear = birthdayThisYear.AddYears(1);
+
+            int daysToBirthday = (birthdayThisYear - today).Days;
+
+            labelResult.Text = $"Разлика в дни: {days} дни\nНавършени години: {years}\nДо следващия рожден ден остават: {daysToBirthday} дни";
+        }
+
+    
+
+        private void buttonClear_Click_1(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today;
+            dateTimePicker2.Value = DateTime.Today;
+            labelResult.Text = "Резултат:";
+        }
+    }
+}
